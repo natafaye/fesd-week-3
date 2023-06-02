411a912545295d0109ee0732fc101971a2caed28
Natalie Childs <nata.faye@gmail.com>
2023-06-01 18:50:31 -0700

    bootstrap and reverse sequence codewars

--- codewars.js
+++ codewars.js
@@ -1,30 +1,19 @@
-// https://www.codewars.com/kata/5892595f190ca40ad0000095/train/javascript
 
-const mispelled = function (word1, word2) {
-    // if they're exactly the same
-    if (word1 === word2) {
-        return true
-    }
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/javascript
 
-    // if one letter added to beginning or end
-    if (Math.abs(word1.length - word2.length) === 1 && (word1.includes(word2) || word2.includes(word1))) {
-        return true
-    }
-
-    // if one letter switched
-    if (word1.length === word2.length) {
-        let differentCounter = 0
-        for (let i = 0; i < word1.length; i++) {
-            if (word1[i] !== word2[i]) {
-                differentCounter++
-                // If we just found our second difference
-                if (differentCounter > 1) {
-                    return false
-                }
-            }
-        }
-        return true
-    }
-
-    return false
-}
+const reverseSeq = n => {
+    let result = []
+    
+  //   for(let i = n; i > 0; i--) {
+  //     result.push(i)
+  //   }
+    
+  //   for(let i = 1; i <= n; i++) {
+  //     result.unshift(i)
+  //   }
+    
+    // cool and tricky map
+    result = Array(n).fill("hello").map( (item, index) => n - index )
+    
+    return result;
+  };
